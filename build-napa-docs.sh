44716a1fd64f1d9cc59b99db4628fe7f58463b4a
Brandon Stafford <brandon@pingswept.org>
2016-03-31 10:57:37 -0400

    Update build-napa-docs.sh

--- build-napa-docs.sh
+++ build-napa-docs.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 
-cd /home/pi/documentation
+cd /home/pi/records
 /opt/gitlab/embedded/bin/git pull origin master
 
-#for i in $(find /home/pi/documentation -maxdepth 1 -type d)
+#for i in $(find /home/pi/records -maxdepth 1 -type d)
 #do
 #    cd "$i"
 #    mkdocs build --clean --site-dir /var/www/snowden.pingswept.org/"$i"
 #done
 
-cd /home/pi/documentation/blue-hour-baltimore
-/usr/local/bin/mkdocs build --clean --site-dir /var/www/snowden.pingswept.org/blue-hour-baltimore
+cd /home/pi/records/blue-hour-baltimore
+/usr/local/bin/mkdocs build --clean --site-dir /var/www/records.pingswept.org/blue-hour-baltimore
 
-cd /home/pi/documentation/color-commons
-/usr/local/bin/mkdocs build --clean --site-dir /var/www/snowden.pingswept.org/color-commons
+cd /home/pi/records/color-commons
+/usr/local/bin/mkdocs build --clean --site-dir /var/www/records.pingswept.org/color-commons
 
-cd /home/pi/documentation/ourself
-/usr/local/bin/mkdocs build --clean --site-dir /var/www/snowden.pingswept.org/ourself
+cd /home/pi/records/ourself
+/usr/local/bin/mkdocs build --clean --site-dir /var/www/records.pingswept.org/ourself
